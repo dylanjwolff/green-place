04bac1a26069938179572057906c5239843c837b
Luca Albinati <>
2019-09-28 01:54:14 +0200

    add hard-coded address lookup

--- greenplace/main.js
+++ greenplace/main.js
@@ -1,8 +1,29 @@
 document.body.style.border = "5px solid red";
 
+let DEFAULT_ID_PREFIX = "green_place_"
+
+class Address {
+    constructor(id, address) {
+        this.id = id
+        this.address = address
+    }
+}
+
 // () -> List(Id)
 function lookUpAddresses() {
+    let elems = document.getElementsByClassName("list-item--address")
 
+    var arr = new Array()
+    let length = elems.length
+    for (var i = 0; i < length; ++i) {
+        elems[i].getElementsByClassName("value")[0].id = DEFAULT_ID_PREFIX + i
+        addr = new Address(
+            elems[i].getElementsByClassName("value")[0].id,
+        elems[i].getElementsByClassName("value")[0].textContent)
+        arr.push(addr)
+    }
+
+    return arr
 }
 
 // List(address) -> List(eco-score)
