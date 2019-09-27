e9ea1db0f105f13aefbf36a5977788261898545c
Luca Albinati <>
2019-09-28 01:14:34 +0200

    setup skeleton

---
+++ greenplace/main.js
@@ -0,0 +1,22 @@
+document.body.style.border = "5px solid red";
+
+// () -> List(Id)
+function lookUpAddresses() {
+
+}
+
+// List(address) -> List(eco-score)
+function computeMetrics() {
+
+}
+
+// List(eco-score) -> ()
+function updateHTML() {
+
+}
+
+
+// add arguments and stuff, etc
+lookUpAddresses()
+computeMetrics()
+updateHTML()
