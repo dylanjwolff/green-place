2b63718e6051347a24420f93c78b9a71502fbd57
wolffd <wolffdy0@gmail.com>
2019-09-29 01:05:57 +0200

    adding grocery poi

--- greenplace/main.js
+++ greenplace/main.js
@@ -7,6 +7,7 @@ import {
     nearby
 } from "./nearby.js"
 
+const GOOGLE_FEATURE_FLAG = false
 let DEFAULT_ID_PREFIX = "green_place_"
 
 var hover_on = false;
@@ -86,6 +87,21 @@ async function computeMetrics(startPlaces, dstPlaces) {
     startPlaces = startPlaces.filter((elem) => {
         return elem.found;
     })
+
+    startPlaces = startPlaces.map( sp => {sp.poi = []; return sp})
+
+    if (GOOGLE_FEATURE_FLAG) {
+            startPlaces.map( sp => {
+                    nearby(sp.lat, sp.lon, "grocery")
+                       .then( res => { sp.poi.push({tag: "grocery", lat: res.lat, lon: res.lng}); return sp })
+                       .catch( e => "poi err" + e )
+            })
+            startPlaces = await Promise.all(startPlaces)
+    } else {
+            startPlaces = startPlaces.map( sp => { sp.poi.push({lat: "47.3723941", lon: "8.5423328", tag: "grocery"}); return sp }) 
+    }
+
+    console.log("startPlaces ", startPlaces)
     console.log("startPlaces is not", startPlaces.length)
     //Compute the distance matrix
     let allPromises = new Array()
@@ -485,6 +501,7 @@ async function createPanel(addresses, address_places, car_boolean) {
 
 // Main
 let startPlaces = lookUpAddresses()
+
 let destPlaces = undefined
 browser.storage.local.get("address_places")
     .then((result) => {
