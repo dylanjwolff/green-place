3ca5086bf35132a4dc51510915b5530635cdb6ce
wolffd <wolffdy0@gmail.com>
2019-09-28 03:08:29 +0200

    adding package.json for npm and eslint config

--- greenplace/main.js
+++ greenplace/main.js
@@ -17,7 +17,7 @@ function lookUpAddresses() {
     let length = elems.length
     for (var i = 0; i < length; ++i) {
         elems[i].getElementsByClassName("value")[0].id = DEFAULT_ID_PREFIX + i
-        addr = new Address(
+        let addr = new Address(
             elems[i].getElementsByClassName("value")[0].id,
         elems[i].getElementsByClassName("value")[0].textContent)
         arr.push(addr)
@@ -38,6 +38,6 @@ function updateHTML(scores) {
 
 
 // add arguments and stuff, etc
-addresses = lookUpAddresses()
-scores = computeMetrics(addresses)
+let addresses = lookUpAddresses()
+let scores = computeMetrics(addresses)
 updateHTML(scores)
